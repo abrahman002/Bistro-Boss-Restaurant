3ce4d82495ccbfa66e297eca091ddfc8b6dd529f
abrahman002 <rahmanx661@gmai.com>
2023-05-24 20:22:29 +0600

    first commit

---
+++ src/Page/Home/Cetagorys/Cetagorys.jsx
@@ -0,0 +1,49 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+
+import slider1 from '../../../assets/home/slide1.jpg'
+import slider2 from '../../../assets/home/slide2.jpg'
+import slider3 from '../../../assets/home/slide3.jpg'
+import slider4 from '../../../assets/home/slide4.jpg'
+import slider5 from '../../../assets/home/slide5.jpg'
+
+const Cetagorys = () => {
+    return (
+        <Swiper
+            slidesPerView={4}
+            spaceBetween={30}
+            centeredSlides={true}
+            pagination={{
+                clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper mb-10"
+        >
+            <SwiperSlide>
+                <img className="rounded" src={slider1} alt="" />
+                <h3 className="text-4xl text-center -mt-20 text-white uppercase">Salads</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+            <img className="rounded" src={slider2} alt="" />
+            <h3 className="text-4xl text-center -mt-20 text-white uppercase">Pizza</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+            <img className="rounded" src={slider3} alt="" />
+            <h3 className="text-4xl text-center -mt-20 text-white uppercase">Soups</h3> 
+            </SwiperSlide>
+            <SwiperSlide>
+            <img className="rounded" src={slider4} alt="" />
+            <h3 className="text-4xl text-center -mt-20 text-white uppercase">Desserts</h3>
+            </SwiperSlide>
+            <SwiperSlide>
+            <img className="rounded" src={slider5} alt="" />
+            <h3 className="text-4xl text-center -mt-20 text-white uppercase">Salads</h3>
+            </SwiperSlide>
+         
+        </Swiper>
+    );
+};
+
+export default Cetagorys;
---
+++ src/Page/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../Banner/Banner";
+import Cetagorys from "../Cetagorys/Cetagorys";
+
+const Home = () => {
+    return (
+        <div>
+         <Banner></Banner>
+         <Cetagorys></Cetagorys>
+        </div>
+    );
+};
+
+export default Home;
